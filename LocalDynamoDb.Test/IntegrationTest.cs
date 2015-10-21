8a2a1e0f7bd5a7e56147688e78478552596af54c
alan-nichols <alan-nichols @ https://github.je-labs.com>
2015-10-21 13:48:56 +0100

    Initial Commit

---
+++ LocalDynamoDb.Test/IntegrationTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon.DynamoDBv2;
+using NUnit.Framework;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
+
+
+namespace LocalDynamoDb.Test
+{
+    [TestFixture]
+    public class IntegrationTest
+    {
+        [Test]
+        public void FullIntegrationTest()
+        {
+            var config = new AmazonDynamoDBConfig { ServiceURL = "http://localhost:8000" };
+            var credentials = new BasicAWSCredentials("A NIGHTINGALE HAS NO NEED FOR KEYS", "IT OPENS DOORS WITH ITS SONG");
+            var client = new AmazonDynamoDBClient(credentials, config);
+
+            var localDynamo = new LocalDynamo();
+
+            try
+            {
+                localDynamo.Start();
+                CreateTestTable(client);
+
+                var tables = client.ListTables();
+                Assert.AreEqual(tables.TableNames.Count, 1,
+                    "Wrong number of tables.  Expected 1 but got " + tables.TableNames.Count);
+            }
+            finally
+            {
+                localDynamo.Stop();
+            }
+        }
+
+        public void CreateTestTable(AmazonDynamoDBClient dynamoClient)
+        {
+            Console.WriteLine("Creating tables.");
+            var request = new CreateTableRequest
+            {
+                TableName = "testTable",
+                AttributeDefinitions = new List<AttributeDefinition>()
+                {
+                    new AttributeDefinition
+                    {
+                        AttributeName = "Id",
+                        AttributeType = "N"
+                    }
+                },
+                KeySchema = new List<KeySchemaElement>()
+                {
+                    new KeySchemaElement
+                    {
+                        AttributeName = "Id",
+                        KeyType = "HASH"
+                    }
+                },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 1,
+                    WriteCapacityUnits = 1
+                }
+            };
+
+            try
+            {
+                dynamoClient.CreateTable(request);
+            }
+            catch (ResourceInUseException ex)
+            {
+                Console.WriteLine("Table already exists.");
+                throw ex;
+            }
+        }
+    }
+}
---
+++ LocalDynamoDb/LocalDynamo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+[assembly: AssemblyVersionAttribute("0.1")]
+namespace LocalDynamoDb
+{
+    public class LocalDynamo
+    {
+        private Process Dynamo { get; set; }
+
+        public LocalDynamo()
+        {
+            Dynamo = Create();
+        }
+
+        private Process Create()
+        {
+            var processJar = new Process();
+            var arguments = "-Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb -inMemory";
+
+            processJar.StartInfo.FileName = "\"" + @"java" + "\"";
+            processJar.StartInfo.Arguments = arguments;
+
+            var rootFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var relativePath = "\\dynamodblocal";
+            string absolutePath = Path.GetFullPath(rootFolder + relativePath);
+            var jarFilePath = absolutePath + "\\DynamoDBLocal.jar";
+
+            if (!File.Exists(jarFilePath))
+            {
+                throw new FileNotFoundException(
+                    "DynamoDBLocal.jar not found in " + absolutePath +
+                    ". Please review the README.txt for setup instructions.",
+                    jarFilePath);
+            }
+
+            processJar.StartInfo.WorkingDirectory = absolutePath;
+            processJar.StartInfo.UseShellExecute = false;
+            processJar.StartInfo.RedirectStandardOutput = true;
+            processJar.StartInfo.RedirectStandardError = true;
+            return processJar;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting in memory DynamoDb");
+            var success = Dynamo.Start();
+            if (!success)
+            {
+                throw new Win32Exception("Error starting dynamo: " + Dynamo.StandardError.ReadToEnd());
+            }
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping in memory DynamoDb");
+            try
+            {
+                Dynamo.Kill();
+            }
+            catch (Win32Exception)
+            {
+                Console.WriteLine(Dynamo.StandardError.ReadToEnd());
+                throw;
+            }
+        }
+    }
+}
